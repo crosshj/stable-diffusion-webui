3675b3be73e02eec1d489d47d740e0d388244393
crosshj <github@chimpjuice.com>
2023-01-11 01:56:24 -0500

    windows11run

--- webui-user.bat
+++ webui-user.bat
@@ -1,8 +1,9 @@
 @echo off
 
-set PYTHON=
+set PYTHON=C:\Users\cross\AppData\Local\Programs\Python\Python310\python.exe
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS=
-
+@REM set COMMANDLINE_ARGS=--disable-safe-unpickle
+@REM set COMMANDLINE_ARGS=--medvram --api --cors-allow-origins=http://127.0.0.1:3456
+set COMMANDLINE_ARGS=--listen
 call webui.bat
--- webui.bat
+++ webui.bat
@@ -1,14 +1,12 @@
 @echo off
 
-if exist webui.settings.bat (
-    call webui.settings.bat
-)
+@REM Fix for not being able to restart
+rmdir /s /q "./venv"
 
 if not defined PYTHON (set PYTHON=python)
-if defined GIT (set "GIT_PYTHON_GIT_EXECUTABLE=%GIT%")
 if not defined VENV_DIR (set "VENV_DIR=%~dp0%venv")
 
-set SD_WEBUI_RESTART=tmp/restart
+
 set ERROR_REPORTING=FALSE
 
 mkdir tmp 2>NUL
@@ -56,14 +54,12 @@ if EXIST %ACCELERATE% goto :accelerate_launch
 
 :launch
 %PYTHON% launch.py %*
-if EXIST tmp/restart goto :skip_venv
 pause
 exit /b
 
 :accelerate_launch
 echo Accelerating
 %ACCELERATE% launch --num_cpu_threads_per_process=6 launch.py
-if EXIST tmp/restart goto :skip_venv
 pause
 exit /b
 
