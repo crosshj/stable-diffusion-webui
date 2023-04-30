e2b366f2d6557e9b1eea2ab2b231fc1544d444ab
crosshj <github@chimpjuice.com>
2023-04-30 00:42:16 -0400

    Update webui-user.bat

--- webui-user.bat
+++ webui-user.bat
@@ -5,5 +5,7 @@ set GIT=
 set VENV_DIR=
 @REM set COMMANDLINE_ARGS=--disable-safe-unpickle
 @REM set COMMANDLINE_ARGS=--medvram --api --cors-allow-origins=http://127.0.0.1:3456
-set COMMANDLINE_ARGS=--listen
+@REM set COMMANDLINE_ARGS=--listen --skip-version-check --no-half --medvram
+set COMMANDLINE_ARGS=--listen --skip-version-check --no-half --xformers --enable-insecure-extension-access
+@REM set COMMANDLINE_ARGS=--listen --skip-version-check --no-half
 call webui.bat
